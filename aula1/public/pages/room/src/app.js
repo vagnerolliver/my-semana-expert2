358416048cea506055c28e050e82b161e2552b35
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-02-07 12:04:43 -0300

    created folder aula 1

--- aula1/public/pages/room/src/app.js
+++ aula1/public/pages/room/src/app.js
@@ -7,7 +7,7 @@ const recordClick = function (recorderBtn) {
     recorderBtn.style.color = this.recordingEnabled ? 'red' : 'white'
   }
 }
-
+ 
 const onload = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const room = urlParams.get('room');
@@ -16,12 +16,15 @@ const onload = () => {
   // const recorderBtn = document.getElementById('record')
   // recorderBtn.addEventListener('click', recordClick(recorderBtn))
 
+  const socketUrl = 'http://localhost:3000'
+  const socketBuilder = new SocketBuilder({ socketUrl })
   const  view = new View()
   const  media = new Media()
   const deps = {
     view,
     media,
     room,
+    socketBuilder
   }
 
   Business.initialize(deps)
--- aula1/server/index.js
+++ aula1/server/index.js
@@ -1,4 +1,4 @@
- const  server = require('http').createServer(( request, response) => {
+ const server = require('http').createServer(( request, response) => {
 
     response.writeHead(204, { 
         'Acess-Control-Allow-Origin': '*',     
@@ -18,17 +18,18 @@
  })
 
  io.on('connection', socket => {
-     console.log('connection', socket.id)
-     socket.on('join-room', (roomId, userId) => {
-         socket.join(roomId);
-         socket.to(roomId).broadcast.emit('user-connected', userId);
-         socket.on('disconnected', () => {
-            console.log('disconnected', roomId, userId);
+    console.log('connection', socket.id)
+    socket.on('join-room', (roomId, userId) => {
+        
+        // adiciona os usuarios na mesma sala
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+        socket.on('disconnect', () => {
+            console.log('disconnected!', roomId, userId)
             socket.to(roomId).broadcast.emit('user-disconnected', userId)
-         })
-     })
- })
-
+        })
+    })
+})
 
 const startServer = () => {
     const { address, port } = server.address()
